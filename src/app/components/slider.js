8b09e69c418be11142023b356287a3588ccd2d69
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-09-28 13:28:05 +0300

    chore: snapshot

--- src/app/components/slider.js
+++ src/app/components/slider.js
@@ -1,9 +1,12 @@
 export class Slider {
   slideCounter = 1
+  transition = 'transform 0.4s ease'
 
   constructor() {
     const slider = document.querySelector('.slider')
     const slides = slider.children
+    const sliderNav = document.querySelector('.slider__nav')
+    const sliderIndicators = sliderNav.children
     const prevSlideButton = document.querySelector('.slider__arrow--left')
     const nextSlideButton = document.querySelector('.slider__arrow--right')
 
@@ -21,23 +24,49 @@ export class Slider {
         this.slideSize = slide[0].borderBoxSize[0].inlineSize
         console.log(this.slideSize, slide)
 
-        this.moveSlider(slider)
+        this.moveSlider(slider, 'none')
       })
 
       resizeObserver.observe(slides[0])
     })
 
     nextSlideButton.addEventListener('click', () => {
-      if (this.slideCounter >= slides.length - 1) return
+      if (this.slideCounter >= slides.length - 1) {
+        sliderIndicators[sliderIndicators.length - 1].classList.remove(
+          'current-slide'
+        )
+        return
+      }
+
+      sliderIndicators[this.slideCounter - 1].classList.remove('current-slide')
 
       this.slideCounter++
+
+      if (slides[this.slideCounter].classList.contains('slider-end')) {
+        sliderIndicators[0].classList.add('current-slide')
+      } else {
+        sliderIndicators[this.slideCounter - 1].classList.add('current-slide')
+      }
+
       this.moveSlider(slider)
     })
 
     prevSlideButton.addEventListener('click', () => {
-      if (this.slideCounter <= 0) return
+      if (this.slideCounter <= 0) {
+        sliderIndicators[0].classList.remove('current-slide')
+        return
+      }
+
+      sliderIndicators[this.slideCounter - 1].classList.remove('current-slide')
 
       this.slideCounter--
+
+      if (slides[this.slideCounter].classList.contains('slider-end')) {
+        sliderIndicators[2].classList.add('current-slide')
+      } else {
+        sliderIndicators[this.slideCounter - 1].classList.add('current-slide')
+      }
+
       this.moveSlider(slider)
     })
 
@@ -55,7 +84,7 @@ export class Slider {
     return clone
   }
 
-  moveSlider(slider, transition = 'transform 0.4s ease') {
+  moveSlider(slider, transition = this.transition) {
     slider.style.transition = transition
     slider.style.transform =
       'translateX(' + -this.slideSize * this.slideCounter + 'px)'
