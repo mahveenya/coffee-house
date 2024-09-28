c541a43b8caee7bc2423e6748da180451f218774
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2024-09-28 23:32:01 +0300

    chore: snapshot

--- src/app/components/slider.js
+++ src/app/components/slider.js
@@ -1,6 +1,9 @@
 export class Slider {
   slideCounter = 1
   transition = 'transform 0.4s ease'
+  startX = 0
+  endX = 0
+  threshold = 50
 
   constructor() {
     const slider = document.querySelector('.slider')
@@ -19,62 +22,37 @@ export class Slider {
     slider.append(firstSlideClone)
     slider.prepend(lastSlideClone)
 
-    window.addEventListener('load', () => {
-      const resizeObserver = new ResizeObserver((slide) => {
-        this.slideSize = slide[0].borderBoxSize[0].inlineSize
-        console.log(this.slideSize, slide)
+    this.setupResizeObserver(slider, slides)
 
-        this.moveSlider(slider, 'none')
-      })
+    slider.addEventListener('touchstart', this.handleTouchStart.bind(this))
+    slider.addEventListener('touchmove', this.handleTouchMove.bind(this))
+    slider.addEventListener(
+      'touchend',
+      this.handleTouchEnd.bind(this, slider, slides, sliderIndicators)
+    )
 
-      resizeObserver.observe(slides[0])
-    })
-
-    nextSlideButton.addEventListener('click', () => {
-      if (this.slideCounter >= slides.length - 1) {
-        sliderIndicators[sliderIndicators.length - 1].classList.remove(
-          'current-slide'
-        )
-        return
-      }
-
-      sliderIndicators[this.slideCounter - 1].classList.remove('current-slide')
-
-      this.slideCounter++
-
-      if (slides[this.slideCounter].classList.contains('slider-end')) {
-        sliderIndicators[0].classList.add('current-slide')
-      } else {
-        sliderIndicators[this.slideCounter - 1].classList.add('current-slide')
-      }
-
-      this.moveSlider(slider)
-    })
-
-    prevSlideButton.addEventListener('click', () => {
-      if (this.slideCounter <= 0) {
-        sliderIndicators[0].classList.remove('current-slide')
-        return
-      }
-
-      sliderIndicators[this.slideCounter - 1].classList.remove('current-slide')
-
-      this.slideCounter--
-
-      if (slides[this.slideCounter].classList.contains('slider-end')) {
-        sliderIndicators[2].classList.add('current-slide')
-      } else {
-        sliderIndicators[this.slideCounter - 1].classList.add('current-slide')
-      }
-
-      this.moveSlider(slider)
-    })
+    nextSlideButton.addEventListener('click', () =>
+      this.changeSlide(slider, slides, sliderIndicators, 'next')
+    )
+    prevSlideButton.addEventListener('click', () =>
+      this.changeSlide(slider, slides, sliderIndicators, 'prev')
+    )
 
     slider.addEventListener('transitionend', () => {
       if (slides[this.slideCounter].classList.contains('slider-end')) {
         this.slideCounter = Math.abs(this.slideCounter - 3)
         this.moveSlider(slider, 'none')
       }
+    })
+  }
+
+  setupResizeObserver(slider, slides) {
+    window.addEventListener('load', () => {
+      const resizeObserver = new ResizeObserver((slide) => {
+        this.slideSize = slide[0].borderBoxSize[0].inlineSize
+        this.moveSlider(slider, 'none')
+      })
+      resizeObserver.observe(slides[0])
     })
   }
 
@@ -86,7 +64,57 @@ export class Slider {
 
   moveSlider(slider, transition = this.transition) {
     slider.style.transition = transition
-    slider.style.transform =
-      'translateX(' + -this.slideSize * this.slideCounter + 'px)'
+    slider.style.transform = `translateX(${-this.slideSize * this.slideCounter}px)`
+  }
+
+  handleTouchStart(event) {
+    this.startX = event.touches[0].clientX
+  }
+
+  handleTouchMove(event) {
+    this.endX = event.touches[0].clientX
+  }
+
+  handleTouchEnd(slider, slides, sliderIndicators) {
+    const distance = this.startX - this.endX
+    if (distance > this.threshold) {
+      this.changeSlide(slider, slides, sliderIndicators, 'next')
+    } else if (distance < -this.threshold) {
+      this.changeSlide(slider, slides, sliderIndicators, 'prev')
+    }
+  }
+
+  changeSlide(slider, slides, sliderIndicators, direction) {
+    if (direction === 'next') {
+      if (this.slideCounter >= slides.length - 1) {
+        sliderIndicators[sliderIndicators.length - 1].classList.remove(
+          'current-slide'
+        )
+        return
+      }
+      this.updateSlideCounter(1, slides, sliderIndicators)
+    }
+
+    if (direction === 'prev') {
+      if (this.slideCounter <= 0) {
+        sliderIndicators[0].classList.remove('current-slide')
+        return
+      }
+      this.updateSlideCounter(-1, slides, sliderIndicators)
+    }
+
+    this.moveSlider(slider)
+  }
+
+  updateSlideCounter(change, slides, sliderIndicators) {
+    sliderIndicators[this.slideCounter - 1].classList.remove('current-slide')
+    this.slideCounter += change
+
+    if (slides[this.slideCounter].classList.contains('slider-end')) {
+      const targetIndex = change > 0 ? 0 : sliderIndicators.length - 1
+      sliderIndicators[targetIndex].classList.add('current-slide')
+    } else {
+      sliderIndicators[this.slideCounter - 1].classList.add('current-slide')
+    }
   }
 }
